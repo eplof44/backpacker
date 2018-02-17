e70e249aedafe59b08c72e2a911a2c523de9a994
Erica <ericaplofsky@gmail.com>
2018-02-17 13:23:22 -0500

    updated items controller to create new items and created form partial for items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,18 +1,24 @@
 class ItemsController < ApplicationController
 
-  before_action :before_item, only: [:show, :edit, :update]
 
-  def index
-  end
+  # def index
+  #   @items = Items.sort_by_value_weight
+  #   @items = Item.all
+  # end
 
   def new
+    @item = Item.new
   end
 
-  def show
-  end
+
 
   def create
+    @item = Item.create(item_params)
+			redirect_to new_item_path(@item)
+  end
 
+
+  def show
   end
 
   def update
@@ -24,12 +30,12 @@ class ItemsController < ApplicationController
   private
 
   def before_item
+
   end
 
   def item_params
+    params.require(:item).permit(:name, :item_weight, :value, :category_id)
+
   end
-
-end
-
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -5,8 +5,8 @@ has_many  :backpacks
 has_many  :trips, through: :backpacks
 
 
- def self.sort_by_value
-   joins(:backpack).order("items.value")
+ def self.sort_by_value_weight
+   items.sort_by{ |i| [t.item_weight, t.value] }  
  end
 
 
