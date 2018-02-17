9eb6b1b4dbcfa2596986ac4aca3b3d35d122ac0e
Erica <ericaplofsky@gmail.com>
2018-02-17 11:29:47 -0500

    initial commit

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,6 @@
+class WelcomeController < ActionController::Base
+
+  def home
+  end
+  
+end
---
+++ db/migrate/20180217160804_add_current_trip_id_to_users.rb
@@ -0,0 +1,5 @@
+class AddCurrentTripIdToUsers < ActiveRecord::Migration[5.1]
+  def change
+    add_column :users, :current_trip_id, :integer
+  end
+end
