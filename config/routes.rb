9efc011c5d44b04b011fb2f41a4fbc0fd6740fc6
Erica <ericaplofsky@gmail.com>
2018-02-25 15:36:05 -0500

    added css. need to fix facebook sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
 
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-  root 'welcome#home'
-  
+devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+
+root 'welcome#home'
+
   resources :items
   resources :backpacks
   resources :trips
