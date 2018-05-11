d055fdf59ecb7e0eec91cb77e57f211c3bc30dc2
Erica Plofsky <ericaplofsky@gmail.com>
2018-05-11 19:46:24 -0400

    added profile page for users to see camping trips and gear

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -1,34 +1,38 @@
-// 
-// //create object of item
-// function Item(item) {
-//   this.id = item.id
-//   this.name = item.name
-//   this.item_weight = item.item_weight
-//   this.value = item.value
-// }
-//
-// //prototype
-//
-// Item.prototype.indexTemplate = function() {
-//     let itemHtml = `
-//     <a href="/items/${ this.id }" data-id="${this.id}">
-//       <h3>${ this.name }</h3>
-//     </a>
-//     `
-//     return itemHtml
-// }
-//
-// $(function () {
-//   $(".all-items").on("click", function(e) {
-//     e.preventDefault()
-//
-//     $.get(this.href + ".json", function(data) {
-//       var $ol = $("div.items-container ol")
-//       $ol.html("")
-//       data.forEach(function(item) {
-//           $ol.append(tripHtml)
-//
-//       });
-//     });
-//   });
-// });
+$(document).ready(function () {
+  getItems()
+})
+
+function Item(item) {
+    this.id = item.id
+    this.name = item.name
+    this.item_weight = item.item_weight
+    this.value = item.value
+}
+
+Item.prototype.indexTemplate = function() {
+    let itemHtml = `
+    <a href="/items/${ this.id }" data-id="${this.id}">
+      <h3>${ item.name}</h3>
+    </a>
+    `
+    return itemHtml
+}
+
+function getItems() {
+    $("a.all-items").on("click", function(e) {
+        e.preventDefault()
+        $('#items-container').html('')
+
+        $.getJSON(this.href, function(itemsData) {
+            renderItems(itemsData)
+        })
+    })
+}
+
+function renderItems(itemsData) {
+    itemsData.forEach(item => {
+        let newItem = new Item(item)
+        let itemHtml = newItem.indexTemplate()
+        $('#items-container').append(itemHtml)
+    })
+}
