4c8a34b6567b625e16e5d1028395a1c23199d562
Erica Plofsky <ericaplofsky@Ericas-iMac.local>
2018-05-11 13:58:56 -0400

    fixed all-trips button to load

--- app/assets/javascripts/trips.js
+++ app/assets/javascripts/trips.js
@@ -19,7 +19,7 @@ function Trip(trip) {
 Trip.prototype.indexTemplate = function() {
     let tripHtml = `
     <a href="/trips/${ this.id }" data-id="${this.id}">
-      <h3>${ this.location }</h3>
+     <li> ${ this.location }</li>
     </a>
     `
     return tripHtml
